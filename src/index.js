0a63c4ff75978419d99206a443810b49a0215e57
seniordev027 <seniordev027@gmail.com>
2023-01-13 10:02:38 +0200

    Change API key

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
