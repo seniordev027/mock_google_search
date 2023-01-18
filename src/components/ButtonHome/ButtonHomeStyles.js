a75c1d8606aa2d284b5a7197362790c99a35c86f
seniordev027 <seniordev027@gmail.com>
2023-01-18 17:00:08 +0200

    Fix button "return home" styles

--- src/components/ButtonHome/ButtonHomeStyles.js
+++ src/components/ButtonHome/ButtonHomeStyles.js
@@ -1,6 +1,13 @@
 const { IconButton, styled } = require("@mui/material");
 
 export const CustomIconButton = styled(IconButton)({
+  zIndex: "10",
   width: "47px",
-  background: "#fff"
+  background: "#fff",
+  "&:hover .MuiTouchRipple-root": {
+    background: "	#1976d2",
+  },
+  "& .MuiSvgIcon-root": {
+    zIndex: 1,
+  }
 });
